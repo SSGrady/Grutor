a6a12a29fd34cb7e52951fc0fec4088755586043
Steven Grady <stevengrady@fb.com>
2022-06-20 16:42:34 -0700

    Register/SignIn Activities implemented.

--- app/src/main/java/com/example/grutor/Activites/LoginActivity.java
+++ app/src/main/java/com/example/grutor/Activites/LoginActivity.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.grutor.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    public Button btnSignUp;
+    public TextView tvSignIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +21,24 @@ public class LoginActivity extends AppCompatActivity {
 
         // hides action bar
         getSupportActionBar().hide();
+        btnSignUp = findViewById(R.id.btnRegister);
+        tvSignIn = findViewById(R.id.tvSignIn);
 
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                 Intent i = new Intent(LoginActivity.this, RegisterActivity.class);
+                 startActivity(i);
+            }
+        });
 
+        tvSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this, SignInActivity.class);
+                startActivity(i);
+            }
+        });
 
         // Intent i = new Intent(this, FeedActivity.class);
         // startActivity(i);
---
+++ app/src/main/java/com/example/grutor/Activites/SignInActivity.java
@@ -0,0 +1,17 @@
+package com.example.grutor.Activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.grutor.R;
+
+public class SignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        // TODO: Connect Parse database and login with a registered parse user
+    }
+}
