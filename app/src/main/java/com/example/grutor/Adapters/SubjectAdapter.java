c4c65b29560855fcc4678ddfbf9ea59381436eb2
Steven Grady <stevengrady@fb.com>
2022-07-19 10:03:29 -0700

    Card UI modifications.

--- app/src/main/java/com/example/grutor/Adapters/SubjectAdapter.java
+++ app/src/main/java/com/example/grutor/Adapters/SubjectAdapter.java
@@ -31,12 +31,6 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.ViewHold
     List<String> titles;
     LayoutInflater inflater;
     Context context;
-    private final String HISTORY_CARD_URL =  "https://static.kanopy.com/cdn-cgi/image/fit=cover,height=540,width=960/https://static-assets.kanopy.com/video-images/5ed68f3d-6522-4a43-bcee-e24c4e84a8d6.jpg";
-    private final String MATH_CARD_URL = "https://p2.piqsels.com/preview/939/797/86/calculator-the-calculation-of-casio-zero.jpg";
-    private final String ENGLISH_CARD_URL = "https://images5.alphacoders.com/394/thumb-1920-394862.jpg";
-    private final String SCIENCE_CARD_URL = "https://images5.alphacoders.com/110/thumb-1920-1107096.jpg";
-    private final String GOVERNMENT_CARD_URL = "https://free4kwallpapers.com/uploads/originals/2021/03/10/united-states-capitol-building-wallpaper.jpg";
-    private final String ECONOMICS_CARD_URL = "https://cutewallpaper.org/21/economy-wallpaper/The-Circular-Economy-Core-Concepts-Explained.-Understand-.jpg";
 
     public SubjectAdapter(Context ctx, List<String> titles) {
         this.titles = titles;
@@ -75,68 +69,56 @@ public class SubjectAdapter extends RecyclerView.Adapter<SubjectAdapter.ViewHold
 
     private void set4KCards(ViewHolder holder) {
         if (holder.title.getText().toString().equals("Math")) {
+            String MATH_CARD_URL = "https://p2.piqsels.com/preview/939/797/86/calculator-the-calculation-of-casio-zero.jpg";
             Glide.with(context).load(MATH_CARD_URL)
                     .into(new SimpleTarget<Drawable>() {
                         @Override
                         public void onResourceReady(@NonNull Drawable resource, @Nullable com.bumptech.glide.request.transition.Transition<? super Drawable> transition) {
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                                // holder.itemView.setBackground(resource);
-                                holder.rlCardImageContainer.setBackground(resource);
-                            }
+                            holder.rlCardImageContainer.setBackground(resource);
                         }});
         } else if (holder.title.getText().toString().equals("English")) {
+            String ENGLISH_CARD_URL = "https://images5.alphacoders.com/394/thumb-1920-394862.jpg";
             Glide.with(context).load(ENGLISH_CARD_URL)
                     .into(new SimpleTarget<Drawable>() {
                         @Override
                         public void onResourceReady(@NonNull Drawable resource, @Nullable com.bumptech.glide.request.transition.Transition<? super Drawable> transition) {
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                                // holder.itemView.setBackground(resource);
-                                holder.rlCardImageContainer.setBackground(resource);
-                            }
+                            holder.rlCardImageContainer.setBackground(resource);
                         }});
         }
         else if (holder.title.getText().toString().equals("Science")) {
+            String SCIENCE_CARD_URL = "https://cdn.shopify.com/s/files/1/0600/9130/2138/files/home-2-hero-1_2560x.jpg?v=1634081389";
             Glide.with(context).load(SCIENCE_CARD_URL)
                     .into(new SimpleTarget<Drawable>() {
                         @Override
                         public void onResourceReady(@NonNull Drawable resource, @Nullable com.bumptech.glide.request.transition.Transition<? super Drawable> transition) {
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                                // holder.itemView.setBackground(resource);
-                                holder.rlCardImageContainer.setBackground(resource);
-                            }
+                            holder.rlCardImageContainer.setBackground(resource);
                         }});
         }
         else if (holder.title.getText().toString().equals("History")) {
+            String HISTORY_CARD_URL = "https://static.kanopy.com/cdn-cgi/image/fit=cover,height=540,width=960/https://static-assets.kanopy.com/video-images/5ed68f3d-6522-4a43-bcee-e24c4e84a8d6.jpg";
             Glide.with(context).load(HISTORY_CARD_URL)
                     .into(new SimpleTarget<Drawable>() {
                         @Override
                         public void onResourceReady(@NonNull Drawable resource, @Nullable com.bumptech.glide.request.transition.Transition<? super Drawable> transition) {
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                                // holder.itemView.setBackground(resource);
-                                holder.rlCardImageContainer.setBackground(resource);
-                            }
+                            holder.rlCardImageContainer.setBackground(resource);
                         }});
         }
         else if (holder.title.getText().toString().equals("Government")) {
+            String GOVERNMENT_CARD_URL = "https://free4kwallpapers.com/uploads/originals/2021/03/10/united-states-capitol-building-wallpaper.jpg";
             Glide.with(context).load(GOVERNMENT_CARD_URL)
                     .into(new SimpleTarget<Drawable>() {
                         @Override
                         public void onResourceReady(@NonNull Drawable resource, @Nullable com.bumptech.glide.request.transition.Transition<? super Drawable> transition) {
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                                // holder.itemView.setBackground(resource);
-                                holder.rlCardImageContainer.setBackground(resource);
-                            }
+                            holder.rlCardImageContainer.setBackground(resource);
                         }});
         }
         else if (holder.title.getText().toString().equals("Economics")) {
+            String ECONOMICS_CARD_URL = "https://cutewallpaper.org/21/economy-wallpaper/The-Circular-Economy-Core-Concepts-Explained.-Understand-.jpg";
             Glide.with(context).load(ECONOMICS_CARD_URL)
                     .into(new SimpleTarget<Drawable>() {
                         @Override
                         public void onResourceReady(@NonNull Drawable resource, @Nullable com.bumptech.glide.request.transition.Transition<? super Drawable> transition) {
-                            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
-                                // holder.itemView.setBackground(resource);
-                                holder.rlCardImageContainer.setBackground(resource);
-                            }
+                            holder.rlCardImageContainer.setBackground(resource);
                         }});
         }
     }
